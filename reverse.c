bc87a29b0caa1b2becb68e4df8746cc2f189865d
computerengineeritu <seyfkutluk@gmail.com>
2020-07-24 17:49:15 +0300

    reverse arrays

---
+++ reverse.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+#include <time.h>
+
+#define SIZE 10
+
+void reverseof(char* arr);
+int main()
+{
+    char* ch;
+    ch=(char*)malloc(SIZE*sizeof(char));
+    printf("Enter");
+    fgets(ch, SIZE, stdin);
+
+    reverseof(ch);
+    printf("%s",ch);
+    return 0;
+}
+void reverseof(char* arr)
+{
+    int i;
+    char temp;
+    int length=strlen(arr);
+    for(i=0; i<length/2; i++){
+        temp=arr[i];
+        arr[i]=arr[length-1-i];
+        arr[length-1-i]=temp;
+    }
+}
